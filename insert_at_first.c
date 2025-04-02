1155eacedd4ce7d2eb5d44f7852bccd953063e48
Rodda Jayaranganath <155821198+RoddaJayaRanganath@users.noreply.github.com>
2025-04-02 14:41:28 +0530

    Add files via upload

---
+++ insert_at_first.c
@@ -0,0 +1,18 @@
+void insert_at_last(struct node **head,int deta)
+{
+    // new node creation;
+    struct node *neww =  (struct node *)malloc(sizeof(struct node));
+    neww->data = deta;
+    neww->next = NULL;
+    
+    // linking the node;
+    if(*head == NULL)
+    {
+        *head = neww;
+    }
+    else
+    {
+        neww->next = *head;
+        *head = neww;
+    }
+}
