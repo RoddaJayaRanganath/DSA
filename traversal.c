b548b959fa238a693161f76fec024c999d5b3d33
Rodda Jayaranganath <155821198+RoddaJayaRanganath@users.noreply.github.com>
2025-04-02 15:16:38 +0530

    traversal.c

--- traversal.c
+++ traversal.c
@@ -3,7 +3,9 @@ void traverse(struct node **temp)
     struct node *ptr = *temp;
     while ( ptr != NULL)
     {
+        // printing the current node data;
         printf("%d->",(ptr->data));
+        // going with the next node;
         ptr = ptr->next;
     }
     printf("NULL");
