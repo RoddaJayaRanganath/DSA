c5dbad38704d825474b7d3de6aa6228259b47424
Rodda Jayaranganath <155821198+RoddaJayaRanganath@users.noreply.github.com>
2025-04-02 15:40:07 +0530

    Update middle_element.c

--- middle_element.c
+++ middle_element.c
@@ -10,7 +10,9 @@ void middle_element(struct node **head)
        //logic;
        while(second != NULL && second->next != NULL)
        {
+           // first node should move one time forward;
            first = first->next;
+           // second node should move two times forward;
            second = second->next->next;
        }
        printf("the middle node : %d",first->data);
